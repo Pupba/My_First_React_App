054e794b670898847cf716324c10c681e6843e83
Pupba <rhdk5148@gmail.com>
2021-09-12 23:29:58 +0900

    Finished My First React App

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(<App />,document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("fromis9"));
 // render is component , component is return html function
 // JSX
-
